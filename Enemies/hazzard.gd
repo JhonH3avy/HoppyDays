45ee0f27159cc0127b97fc4218787cb70305c93a
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2019-06-22 20:29:50 -0500

    Spikeman NPC

--- Enemies/hazzard.gd
+++ Enemies/hazzard.gd
@@ -2,5 +2,5 @@ extends Area2D
 
 
 
-func _on_spikes_top_body_entered(body):
+func _on_hazzard_body_entered(body):
 	get_tree().call_group("GameState", "hurt")
--- Player/Player.gd
+++ Player/Player.gd
@@ -44,7 +44,7 @@ func animate():
 func apply_gravity():
 	if (position.y > WORLD_LIMIT):
 		get_tree().call_group("GameState", "fall_out")
-	if is_on_floor():
+	if is_on_floor() and motion.y > 0:
 		motion.y = 0
 	elif is_on_ceiling():
 		motion.y = GRAVITY
